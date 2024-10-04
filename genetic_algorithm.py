7899f11e1704c68840395a62129e65608380bbb4
Ana Flavia Moreira Pires <128156301+ana-flav@users.noreply.github.com>
2024-10-03 22:26:23 -0300

    fix: arrumando trem

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,18 +3,6 @@ import random
 
 class GeneticAlgorithm:
     def __init__(self, T, S, CN, YB, PS, MP, EN, SP):
-        """
-        :param T: Número de tiers
-        :param S: Número de stacks
-        :param CN: Número de contêineres
-        :param YB: Matriz Yard Bay
-        :param PS: Tamanho da população
-        :param MP: Taxa de mutação
-        :param EN: Número de evoluções
-        :param SP: Tamanho do torneio
-        :GN: Ajuste conforme o artigo manda, tamanho do cromossomo
-        :population: vetor de população
-        """
         self.T = T  # Número de tiers
         self.S = S  # Número de stacks
         self.CN = CN  # Número de contêineres
@@ -23,7 +11,7 @@ class GeneticAlgorithm:
         self.MP = MP  # Taxa de mutação
         self.EN = EN  # Número de evoluções
         self.SP = SP  # Tamanho do torneio
-        self.GN = CN * 10  # Ajuste conforme o artigo manda
+        self.GN = CN * 10 # Ajuste conforme o artigo manda
         self.population = []  
 
     def initialize_population(self):
@@ -32,6 +20,7 @@ class GeneticAlgorithm:
     def evaluate_population(self):
         for individual in self.population:
             individual.evaluate_fitness(self.T, self.S, self.CN, self.GN, self.YB)
+            
 
     def selection(self):
         selected = []
@@ -40,15 +29,17 @@ class GeneticAlgorithm:
             best_individual = min(tournament, key=lambda ind: ind.fitness)
             selected.append(best_individual)
         return selected
-        
-    def elitism(self):
-        best_individual = min(self.population, key=lambda ind: ind.fitness)
-        return best_individual
-        
+
     def crossover(self, parent1, parent2):
-        crossPoint = random.randint(1, self.GN - 2)
-        child1_chromosome = parent1.chromosome[:crossPoint] + parent2.chromosome[crossPoint:]
-        child2_chromosome = parent2.chromosome[:crossPoint] + parent1.chromosome[crossPoint:]
+        child1_chromosome = []
+        child2_chromosome = []
+        for i in range(self.GN):
+            if random.random() < 0.5:
+                child1_chromosome.append(parent1.chromosome[i])
+                child2_chromosome.append(parent2.chromosome[i])
+            else:
+                child1_chromosome.append(parent2.chromosome[i])
+                child2_chromosome.append(parent1.chromosome[i])
         child1 = Individual(self.GN, self.S)
         child2 = Individual(self.GN, self.S)
         child1.chromosome = child1_chromosome
@@ -60,10 +51,13 @@ class GeneticAlgorithm:
             if random.random() <= self.MP:
                 individual.chromosome[i] = random.randint(0, self.S - 1)
 
+
     def run(self):
 
         self.initialize_population()
         self.evaluate_population()
+
+        best_fitness = []   
 
         for _ in range(self.EN):
             P_estrela = []  
@@ -74,14 +68,18 @@ class GeneticAlgorithm:
                 C1_new, C2_new = self.crossover(selected_parents[0], selected_parents[1]) 
                 P_estrela.append(C1_new)
                 P_estrela.append(C2_new)
-            best = self.elitism()
-            P_estrela.append(best)
+
             for i in range(self.PS):
                 self.mutate(P_estrela[i])
 
             self.population = P_estrela
             self.evaluate_population()
 
-        best_individual = min(self.population, key=lambda ind: ind.fitness)
+            best_individual_generation = min(self.population, key=lambda ind: ind.fitness)
+            best_fitness.append(best_individual_generation.fitness)
+            print(f"Geração {_}: Melhor Fitness = {best_fitness[-1]}")
 
+
+        best_individual = min(self.population, key=lambda ind: ind.fitness)
+        
         return best_individual.chromosome, best_individual.fitness
